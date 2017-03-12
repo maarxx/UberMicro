68150a3a269112cf692dbdc7de9134f4cbce1059
maarxx <chris.lewark@gmail.com>
2017-03-12 10:47:58 -0400

    Added basic MainTab with toggle button

--- UberMicro/MainTabDef_UberMicro.cs
+++ UberMicro/MainTabDef_UberMicro.cs
@@ -4,14 +4,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using UnityEngine;
+using Verse;
 
 namespace UberMicro
 {
     public class MainTabWindow_UberMicro : MainTabWindow
     {
+        public MainTabWindow_UberMicro()
+        {
+            //base.forcePause = true;
+        }
+
+        public override Vector2 InitialSize
+        {
+            get
+            {
+                //return base.InitialSize;
+                return new Vector2(200f, 200f);
+            }
+        }
+
+        public override MainTabWindowAnchor Anchor =>
+            MainTabWindowAnchor.Right;
+
         public override void DoWindowContents(Rect canvas)
         {
             base.DoWindowContents(canvas);
+
+            Rect topRow = new Rect(canvas);
+            topRow.height = 50f;
+
+            //Rect topRowLeft = new Rect(topRow);
+            //topRowLeft.width = 200f;
+
+            bool curMode = Find.VisibleMap.GetComponent<MapComponent_UberMicro>().enabled;
+            string buttonLabel = "Current Mode is: ";
+            if (curMode)
+            {
+                buttonLabel += "ON";
+            }
+            else
+            {
+                buttonLabel += "OFF";
+            }
+            if ( Widgets.ButtonText(topRow, buttonLabel) )
+            {
+                Find.VisibleMap.GetComponent<MapComponent_UberMicro>().enabled = !curMode;
+            }
         }
+
     }
 }
