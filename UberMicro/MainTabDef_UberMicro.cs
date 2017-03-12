735da23af49f7a2c1a39496e46f4a326f7f5e6e0
maarxx <chris.lewark@gmail.com>
2017-03-11 23:25:19 -0500

    Proof of concept

--- UberMicro/MainTabDef_UberMicro.cs
+++ UberMicro/MainTabDef_UberMicro.cs
@@ -3,10 +3,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using UnityEngine;
 
 namespace UberMicro
 {
     public class MainTabWindow_UberMicro : MainTabWindow
     {
+        public override void DoWindowContents(Rect canvas)
+        {
+            base.DoWindowContents(canvas);
+        }
     }
 }
--- UberMicro/MapComponent_UberMicro.cs
+++ UberMicro/MapComponent_UberMicro.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RimWorld.Planet;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +9,40 @@ namespace UberMicro
 {
     class MapComponent_UberMicro : MapComponent
     {
+        public bool enabled;
+        private Dictionary<Pawn, string> prioritizedJobs;
+
         public MapComponent_UberMicro(Map map) : base(map)
         {
+            this.enabled = true;
+            this.prioritizedJobs = new Dictionary<Pawn, string>();
+        }
 
+        public override void MapComponentTick()
+        {
+            if (enabled)
+            {
+                foreach (Pawn p in map.mapPawns.FreeColonistsSpawned)
+                {
+                    if (p.CurJob.playerForced)
+                    {
+                        prioritizedJobs[p] = p.CurJob.ToString();
+                    }
+                    else
+                    {
+                        if (prioritizedJobs[p] != null)
+                        {
+                            Find.LetterStack.ReceiveLetter(
+                                    "Job Done",
+                                    p.NameStringShort + " has completed their prioritized job to: " + prioritizedJobs[p],
+                                    LetterType.BadUrgent,
+                                    new GlobalTargetInfo(p)
+                                );
+                            prioritizedJobs[p] = null;
+                        }
+                    }
+                }
+            }
         }
     }
 }
